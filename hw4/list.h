5869f39256238cd40c25a3f255d499e1c762bc77
BreslinIT <pokemax209@gmail.com>
2022-08-04 16:29:08 -0700

    changed list.h

--- hw4/list.h
+++ hw4/list.h
@@ -1,45 +1,40 @@
 #pragma once
-#include "node.h"
+#include "word.h"
 
 class List {
 public:
-    // General Class Elements Required in CS 162
-    List();                       // Default constructor
-    ~List();                       // Default destructor
-    List(const List &);           // Copy constructor
-    List(List &&);                // Move constructor
-    List(Node *);              // Parm constructor
-    List & operator = (const List &);// Copy assignment
-    List & operator = (List &&);  // Move assignment
+    List();
+    List(const List &);
+    ~List();
+    List& operator=(const List &);
+    
 
-    friend std::ostream & operator << (std::ostream &, const List &);
-    friend std::istream & operator >> (std::istream &, List &);
+    // Simply returns this->size
+    size_t length() const;
+    // Returns a reference to the word at the 
+    // passed location in this->index
+    Word & get_index(const int &) const;
 
-    // List ADT Elements
-    bool is_empty() const;                  // True if list is empty
-    bool is_full() const;                   // True if list is full
-    Node & operator[](std::size_t);                // Reference index
-    const Node & operator[](std::size_t) const;    // Value index
-    const Node & at(std::size_t);                  // Accessor
-    Node * find(Node *);                        // Search
-    bool remove(std::size_t);                    // Positional deletion
-    bool remove(Node *);                      // Value deletion (multiple deletions)
-    bool insert(Node *, size_t);              // Positional insertion
-    bool insert(Node *);                      // Ordered insertion
-    bool erase();                                // Erase all elements
-    size_t length() const;                         // Number of elements in list
+    // Inserts a word at the back of this->index
+    void insert(const Word &);
+    // Returns the location of the first word in 
+    // this->index with a data member variable which
+    // matches the passed c-string or this->size
+    // if no matching word is found
+    int contains(char * const &) const;
+    // Same as above, but a Word object is passed
+    // instead of a c-string
+    int contains(const Word &) const;
+    // Prints the contents of this->index
+    void print() const;
+    // Sorts this->index alphabetically
+    void sort();
 
 private:
-    size_t size;               // Size of the list
-    size_t capacity;           // Capacity of the list
-    Node * head;               // Head pointer
-    Node * tail;               // Tail pointer
+    struct Node {
+		Word * data;
+		Node * next;
+	};
+	Node * index;
+    size_t size;
 };
-
-          
-bool operator <  (const List &, const List &);
-bool operator <= (const List &, const List &);
-bool operator == (const List &, const List &);
-bool operator != (const List &, const List &);
-bool operator >= (const List &, const List &);
-bool operator >  (const List &, const List &);
